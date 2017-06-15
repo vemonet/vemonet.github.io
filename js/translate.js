762ed39b14d439e1eebde710bef93fbcf5c1db40
Vincent Emonet <vincent.emonet@gmail.com>
2017-06-15 04:42:07 +0200

    add an example of translating

---
+++ js/translate.js
@@ -0,0 +1,23 @@
+var app = angular.module('translate', ['pascalprecht.translate']);
+
+app.config(function ($translateProvider) {
+  $translateProvider.translations('en', {
+    TITLE: 'Hello',
+    FOO: 'This is a paragraph.',
+    BUTTON_LANG_EN: 'english',
+    BUTTON_LANG_DE: 'german'
+  });
+  $translateProvider.translations('de', {
+    TITLE: 'Hallo',
+    FOO: 'Dies ist ein Paragraph.',
+    BUTTON_LANG_EN: 'englisch',
+    BUTTON_LANG_DE: 'deutsch'
+  });
+  $translateProvider.preferredLanguage('en');
+});
+
+app.controller('Ctrl', function ($scope, $translate) {
+  $scope.changeLanguage = function (key) {
+    $translate.use(key);
+  };
+});
