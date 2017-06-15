32778c44580a7cf7c1f98fcae817c58272b049dc
Vincent Emonet <vincent.emonet@gmail.com>
2017-06-15 13:49:48 +0200

    add first multilang entry to resume timeline

--- js/translate.js
+++ js/translate.js
@@ -1,15 +1,20 @@
 var app = angular.module('translate', ['pascalprecht.translate']);
 
 app.config(function ($translateProvider) {
+  // Dictionaries
   $translateProvider.translations('en', {
     TITLE: 'Hello',
     FOO: 'This is a paragraph.',
+    FIRST_EXPE_TITLE: "Semantic Web design engineer",
+    FIRST_EXPE_TXT: "BioPortal Project (https://github.com/sifrproject)<ul><li>Administration and development of a local instance of BioPortal (NCBO ontologies repository ) for SIFR et IBC projects (http://bioportal.lirmm.fr et http://agroportal.lirmm.fr)</li><li>Feature development (ontologies metadata, text semantic annotation, multilingual annotation)</li><li> Put into production new prototypes within the platform</li></ul>",
     BUTTON_LANG_EN: 'english',
     BUTTON_LANG_DE: 'german'
   });
   $translateProvider.translations('fr', {
     TITLE: 'Salut',
     FOO: 'En français.',
+    FIRST_EXPE_TITLE: "Ingénieur d'étude Web sémantique",
+    FIRST_EXPE_TXT: "Projet BioPortal (https://github.com/sifrproject)<ul><li>Administration et modification d'une instance locale de BioPortal (répertoire d'ontologies du NCBO) pour le projet SIFR et IBC (http://bioportal.lirmm.fr et http://agroportal.lirmm.fr)</li><li>Développement de fonctionnalités (metadata des ontologies, annotation multilingue)</li><li>Mise en production de nouveau prototypes au sein de la plateforme</li></ul>",
     BUTTON_LANG_EN: 'englisch',
     BUTTON_LANG_DE: 'deutsch'
   });
