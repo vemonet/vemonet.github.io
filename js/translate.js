2ef412851d1285382305b7d9f83fe490cff598dd
Vincent Emonet <vincent.emonet@gmail.com>
2017-06-19 13:06:23 +0200

    add a translation for about me title

--- js/translate.js
+++ js/translate.js
@@ -8,6 +8,7 @@ app.config(function ($translateProvider) {
     DDL_CV_LABEL: 'Resume',
     DDL_CV_FILE: 'resources/RESUME_VincentEmonet.pdf',
     
+    ABOUT_ME_H2: "About me",
     ABOUT_TEXT: `I am actually working as a research engineer at the LIRMM (Laboratory of Informatics, Robotics, Microelectronics of Montpellier). On different project linked to the semantic Web and ontologies:
 <ul>
   <li>SIFR BioPortal and AgroPortal web services: two web ontology repositories allowing users to annotate text with the stored ontologies.</li>
@@ -98,6 +99,7 @@ Skills: Python, DNA sequence analysis
     DDL_CV_LABEL: 'CV',
     DDL_CV_FILE: 'resources/CV_VincentEmonet.pdf',
     
+    ABOUT_ME_H2: 'A propos',
     ABOUT_TEXT: `Je travaille actuellement en tant qu'ingénieur d'étude au LIRMM (Laboratoire d'informatique, de Robotique, de Microélectronique de Montpellier) sur plusieurs projets liés au Web sémantique : 
 <ul>
   <li>AgroPortal et BioPortal : deux portails d'ontologies permettant d'exploiter les ontologies stockées et d'annoter des données.</li>
